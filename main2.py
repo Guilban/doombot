40bf63f2fb71f922c811f98c32eb9ec183d78f28
Guilban <javier.leon.chunchito@gmail.com>
2024-04-30 20:55:36 -0400

    cambio a linux en aws

--- main2.py
+++ main2.py
@@ -8,19 +8,27 @@ from youtubesearchpython import VideosSearch
 import asyncio
 
 
+TOKEN=''
 
+
+ydl_op = {
+        'format' : 'bestaudio/best',
+    }
+FFMPEG_OPTIONS = {'options': '-vn'}
+ydl = YoutubeDL(ydl_op)
+lista=[]
 
 intents = discord.Intents.all()
 bot=commands.Bot(command_prefix='.',intents=intents)
-global Lista_num
-Lista_num=0
+
 
 def search_yt(item):
     if item.startswith("https://"):
-        return item
+        title = ydl.extract_info(item, download=False)["title"]
+        return{'URL':item, 'titulo':title}
     search = VideosSearch(item, limit=1)
     
-    return search.result()["result"][0]["link"]
+    return{'URL':search.result()["result"][0]["link"], 'titulo':search.result()["result"][0]["title"]}
 
 @bot.event
 async def on_ready():
@@ -58,10 +66,9 @@ async def desconectar(ctx):
 
 @bot.command(pass_context = True)
 async def play(ctx, *args):
-
     
-
-    global Lista_num
+    global lista
+    
     query= " ".join(args)
     song = search_yt(query)
     #CONECTAR CANAL
@@ -81,110 +88,30 @@ async def play(ctx, *args):
 
 
     def revisar_lista():
-        LR_en_archivo = os.path.isdir("./Lista")
-        if LR_en_archivo is True:
-            DIR = os.path.abspath(os.path.realpath("Lista"))
-            tamaño = len(os.listdir(DIR))
-            c_activa = tamaño - 1
-            try:
-                c_primera = os.listdir(DIR)[0]
-            except:
-                print("No hay canciones\n")
-                listar.clear()
-                return
-            localizacion_principal = os.path.dirname(os.path.realpath(__file__))
-            c_localizacion = os.path.abspath(os.path.realpath("Lista") + "\\" + c_primera)
-            if tamaño != 0:
-                print("Cancion lista, se reproducira en breve")
-                print(f"Canciones en la lista: {c_activa}")
-                c_encontrada = os.path.isfile("cancion.mp3")
-                if c_encontrada:
-                    os.remove("cancion.mp3")
-                    shutil.move(c_localizacion,localizacion_principal)
-                    for file in os.listdir("./"):
-                        if file.endswith(".mp3"):
-                            os.rename(file,'cancion.mp3')
-                    voice.play(discord.FFmpegPCMAudio("cancion.mp3"),after = lambda e:revisar_lista())
-                else:
-                    listar.clear()
-                    return
-            else:
-                listar.clear()
-                print("No se agrego a la lista")
+        if len(lista) > 0:
+            lista.pop(0)
+            cancion=lista[0]
+            voice.play(discord.FFmpegPCMAudio(cancion,executable='ffmpeg',**FFMPEG_OPTIONS),after=lambda e: revisar_lista())
     
-    c_encontrada = os.path.isfile("cancion.mp3")
-    try:#reproducir cancion
-        if c_encontrada:
-            os.remove("cancion.mp3")
-            listar.clear()
-            print("removido archivo antiguo")
-    except PermissionError:#agregar a la lista
-        Cancion_lista = os.path.isdir("./Lista")
-        if Cancion_lista is False:
-            os.mkdir("Lista")
-        DIR = os.path.abspath(os.path.realpath("Lista"))
-        #Lista_num = len(os.listdir(DIR)) no funciona sobrescribe canciones cambiado por variable global
-        
-        Lista_num+=1
-        agregar_lista = True
-        while agregar_lista:
-            if Lista_num is listar:
-                Lista_num+=1
-            else:
-                agregar_lista = False
-                listar[Lista_num] = Lista_num
-        Lista_path = os.path.abspath(os.path.realpath("Lista") + f"\cancion{Lista_num}.%(ext)s")
-        ydl_op = {
-            'format':'bestaudio/best',
-            'quiet' : True,
-            'outtmpl' : Lista_path,
-            'ffmpeg_location':os.path.realpath('C:\\ffmpeg\\bin\\ffmpeg.exe'),
-            'postprocessors' : [{
-                'key' : 'FFmpegExtractAudio',
-                'preferredcodec' : 'mp3',
-                'preferredquality' : '192',
-            }],
-        }
+    if len(lista)>0:
+        data =  ydl.extract_info(song, download=False)
+        lista.append(data)
 
-
-        with YoutubeDL(ydl_op) as ydl:
-            print("Descargar Cancion")
-            ydl.download([song])
-
-        await ctx.send("Añadida la cancion "+str(Lista_num)+" a la lista de reproduccion")\
+        await ctx.send("Añadida la cancion "+str(data['titulo'])+" a la lista de reproduccion")\
         
         print("cancion añadida")
         return
     #
-    LR_en_archivo = os.path.isdir("./Lista")
-    try:
-        LR_carpeta = "./Lista"
-        if LR_en_archivo is True:
-            print("Removida la carpeta antigua")
-            shutil.rmtree(LR_carpeta)
-    except:
-        print("no hay carpeta antigua")
     
     await ctx.send("Todo lsito")
 
     voice = get(bot.voice_clients,guild = ctx.guild)
+  
+    data=  ydl.extract_info(song, download=False)
+    cancion=data['url']
+    voice.play(discord.FFmpegPCMAudio(cancion,executable='ffmpeg',**FFMPEG_OPTIONS),after=lambda e: revisar_lista())
 
-    ydl_op = {
-        'format' : 'bestaudio/best',
-        'ffmpeg_location':os.path.realpath('C:\\ffmpeg\\bin\\ffmpeg.exe'),
-    }
-
-    #with YoutubeDL(ydl_op) as ydl:
-        #print("descargando cancion\n")
-       # ydl.download([song])
-    FFMPEG_OPTIONS = {'options': '-vn'}
-    with YoutubeDL(ydl_op) as ydl:
-        
-        data=  ydl.extract_info(song, download=False)
-        cancion=data['url']
-    voice.play(discord.FFmpegPCMAudio(cancion,executable='ffmpeg.exe',**FFMPEG_OPTIONS),after=lambda e: revisar_lista())
-
-    #await ctx.send(f"reproduciendo: {nombre[0]}")
+    await ctx.send(f"reproduciendo: {data['titulo']}")
     print("Reproduciendo \n")
 
             
